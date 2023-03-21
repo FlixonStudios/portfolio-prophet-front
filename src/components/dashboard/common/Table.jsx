cade29267cc5dd73e82d96ff9c32ecf29e98c3f3
FlixonStudios <chiakaiming@gmail.com>
2023-03-21 11:11:30 +0800

    Use styled component for Table

--- src/components/dashboard/common/Table.jsx
+++ src/components/dashboard/common/Table.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledTable = styled.div`
+    display: flex,
+    flexDirection: column,
+`
 
 function Table({ headers, rows, title = 'table' }) {
     return (
         <>
-            <div className="d-flex flex-column dash-card-block">
+            <StyledTable>
                 <div className="list--title">{title}</div>
                 <div className="card forecast-data-table list--value mr-0">
                     <table>
                         <thead>
-                            <tr>
-                                {headers} 
-                            </tr>
+                            <tr>{headers}</tr>
                         </thead>
-                        <tbody>
-                                {rows}
-                        </tbody>
+                        <tbody>{rows}</tbody>
                     </table>
                 </div>
-            </div>
+            </StyledTable>
         </>
     )
 }
